2e845a27d8d373f323dde9443e32f74ab9607533
Dmitriy Stupak <ds@bam-boo.eu>
2026-07-24 15:08:50 +0300

    implemented navigation 3 + serialization libs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,6 +42,12 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            // navigation 3
+            implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.jetbrains.lifecycle.viewmodel.nav3)
+            implementation(libs.jetbrains.lifecycle.viewmodel)
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
