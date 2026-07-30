614e37b2510bf2dfcbebcaf1e6dd6ad81f0bae3c
Dmitriy Stupak <ds@bam-boo.eu>
2026-07-30 13:58:46 +0300

    use navigation in App fun

--- composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/App.kt
+++ composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/App.kt
@@ -3,12 +3,12 @@ package com.matatoa.wheremystuff
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.matatoa.wheremystuff.designsystem.theme.WhereMyStuffTheme
-import com.matatoa.wheremystuff.presentation.startscreen.StartScreenRoot
+import com.matatoa.wheremystuff.navigation.AppNavigation
 
 @Composable
 @Preview
 fun WhereMyStuff() {
     WhereMyStuffTheme {
-        StartScreenRoot()
+        AppNavigation()
     }
 }
--- composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/Constants.kt
@@ -2,3 +2,4 @@ package com.matatoa.wheremystuff
 
 const val EMPTY_STRING = ""
 const val NEW_PLACE_LENGTH = 30
+const val STOP_TIME_OUT_MILLIS = 5000L
