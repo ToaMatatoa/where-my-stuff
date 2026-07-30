4c2076f1d941c8f0c504b36b6725cccd204dc417
Dmitriy Stupak <ds@bam-boo.eu>
2026-07-30 14:00:08 +0300

    getting place details by its id

--- core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/database/dao/PlaceDao.kt
+++ core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/database/dao/PlaceDao.kt
@@ -13,4 +13,8 @@ interface PlaceDao {
 
     @Query(value = "SELECT * FROM place ORDER BY id")
     fun getAllPlaces(): Flow<List<PlaceEntity>>
+
+    /** Emits `null` once the place is deleted or if it never existed. */
+    @Query(value = "SELECT * FROM place WHERE id = :id")
+    fun getPlaceDetails(id: Int): Flow<PlaceEntity?>
 }
--- core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/repository/PlaceRepository.kt
+++ core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/repository/PlaceRepository.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface PlaceRepository {
     fun getAllPlaces(): Flow<List<PlaceEntity>>
+    fun getPlaceDetails(id: Int): Flow<PlaceEntity?>
     suspend fun addPlace(place: PlaceEntity)
 }
 
@@ -16,6 +17,9 @@ class PlaceRepositoryImpl(
 ) : PlaceRepository {
     override fun getAllPlaces(): Flow<List<PlaceEntity>> =
         placeDataSource.getAllPlaces()
+
+    override fun getPlaceDetails(id: Int): Flow<PlaceEntity?> =
+        placeDataSource.getPlaceDetails(id = id)
 
     override suspend fun addPlace(place: PlaceEntity) =
         mutablePlaceDataSource.addPlace(place = place)
