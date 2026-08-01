2e7d5c061ad692a070e9309aaa5e71ab12f40092
Dmitriy Stupak <ds@bam-boo.eu>
2026-08-01 23:30:56 +0300

    iOS, desktop app icon

--- composeApp/src/desktopMain/kotlin/com/matatoa/wheremystuff/main.kt
+++ composeApp/src/desktopMain/kotlin/com/matatoa/wheremystuff/main.kt
@@ -3,13 +3,17 @@ package com.matatoa.wheremystuff
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.matatoa.wheremystuff.di.initKoin
+import org.jetbrains.compose.resources.painterResource
+import wheremystuff.composeapp.generated.resources.Res
+import wheremystuff.composeapp.generated.resources.app_icon
 
 fun main() {
     initKoin()
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = "WhereMyStuff",
+            title = "Where My Stuff",
+            icon = painterResource(Res.drawable.app_icon),
         ) {
             WhereMyStuff()
         }
