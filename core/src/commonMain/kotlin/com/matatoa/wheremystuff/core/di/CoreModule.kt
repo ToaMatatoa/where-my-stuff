ea2c82f69c96b0017abdb28ebe0ab91a9e8ab48f
Dmitriy Stupak <ds@bam-boo.eu>
2026-08-01 15:29:19 +0300

    updated core di, db schema v2

--- core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/di/CoreModule.kt
+++ core/src/commonMain/kotlin/com/matatoa/wheremystuff/core/di/CoreModule.kt
@@ -2,12 +2,26 @@ package com.matatoa.wheremystuff.core.di
 
 import com.matatoa.wheremystuff.core.database.getPlaceDao
 import com.matatoa.wheremystuff.core.database.getRoomDatabase
+import com.matatoa.wheremystuff.core.database.getStuffDao
+import com.matatoa.wheremystuff.core.database.getSubPlaceDao
 import com.matatoa.wheremystuff.core.datastore.MutablePlaceDataSource
 import com.matatoa.wheremystuff.core.datastore.MutablePlaceDataSourceImpl
+import com.matatoa.wheremystuff.core.datastore.MutableStuffDataSource
+import com.matatoa.wheremystuff.core.datastore.MutableStuffDataSourceImpl
+import com.matatoa.wheremystuff.core.datastore.MutableSubPlaceDataSource
+import com.matatoa.wheremystuff.core.datastore.MutableSubPlaceDataSourceImpl
 import com.matatoa.wheremystuff.core.datastore.PlaceDataStore
 import com.matatoa.wheremystuff.core.datastore.PlaceDataStoreImpl
+import com.matatoa.wheremystuff.core.datastore.StuffDataStore
+import com.matatoa.wheremystuff.core.datastore.StuffDataStoreImpl
+import com.matatoa.wheremystuff.core.datastore.SubPlaceDataStore
+import com.matatoa.wheremystuff.core.datastore.SubPlaceDataStoreImpl
 import com.matatoa.wheremystuff.core.repository.PlaceRepository
 import com.matatoa.wheremystuff.core.repository.PlaceRepositoryImpl
+import com.matatoa.wheremystuff.core.repository.StuffRepository
+import com.matatoa.wheremystuff.core.repository.StuffRepositoryImpl
+import com.matatoa.wheremystuff.core.repository.SubPlaceRepository
+import com.matatoa.wheremystuff.core.repository.SubPlaceRepositoryImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -16,6 +30,8 @@ expect fun platformModule(): Module
 val provideDatabaseModule = module {
     single { getRoomDatabase(builder = get()) }
     single { getPlaceDao(whereMyStuffDatabase = get()) }
+    single { getSubPlaceDao(whereMyStuffDatabase = get()) }
+    single { getStuffDao(whereMyStuffDatabase = get()) }
 }
 
 val provideDataSourceModule = module {
@@ -27,11 +43,33 @@ val provideDataSourceModule = module {
             placeDao = get()
         )
     }
+    single<SubPlaceDataStore> {
+        SubPlaceDataStoreImpl(subPlaceDao = get())
+    }
+    single<MutableSubPlaceDataSource> {
+        MutableSubPlaceDataSourceImpl(
+            subPlaceDao = get()
+        )
+    }
+    single<StuffDataStore> {
+        StuffDataStoreImpl(stuffDao = get())
+    }
+    single<MutableStuffDataSource> {
+        MutableStuffDataSourceImpl(
+            stuffDao = get()
+        )
+    }
 }
 
 val provideRepositoryModule = module {
     single<PlaceRepository> {
         PlaceRepositoryImpl(placeDataSource = get(), mutablePlaceDataSource = get())
+    }
+    single<SubPlaceRepository> {
+        SubPlaceRepositoryImpl(subPlaceDataSource = get(), mutableSubPlaceDataSource = get())
+    }
+    single<StuffRepository> {
+        StuffRepositoryImpl(stuffDataSource = get(), mutableStuffDataSource = get())
     }
 }
 
