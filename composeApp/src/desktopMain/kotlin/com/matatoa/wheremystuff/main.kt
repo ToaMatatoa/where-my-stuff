5557b19c89b9a482e91ab0c9b3142e74c3bc0cf3
Dmitriy Stupak <ds@bam-boo.eu>
2026-08-02 00:20:45 +0300

    iOS/desktop splash screen

--- composeApp/src/desktopMain/kotlin/com/matatoa/wheremystuff/main.kt
+++ composeApp/src/desktopMain/kotlin/com/matatoa/wheremystuff/main.kt
@@ -1,8 +1,14 @@
 package com.matatoa.wheremystuff
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.matatoa.wheremystuff.di.initKoin
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 import wheremystuff.composeapp.generated.resources.Res
 import wheremystuff.composeapp.generated.resources.app_icon
@@ -15,7 +21,17 @@ fun main() {
             title = "Where My Stuff",
             icon = painterResource(Res.drawable.app_icon),
         ) {
-            WhereMyStuff()
+            var showSplash by remember { mutableStateOf(true) }
+            LaunchedEffect(Unit) {
+                delay(SPLASH_DURATION_MS)
+                showSplash = false
+            }
+
+            if (showSplash) {
+                SplashScreen()
+            } else {
+                WhereMyStuff()
+            }
         }
     }
 }
