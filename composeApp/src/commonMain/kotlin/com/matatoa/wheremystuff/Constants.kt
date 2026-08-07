b34fad5f114cbef7117db7b3efb6b51c6af7f819
Dmitriy Stupak <ds@bam-boo.eu>
2026-08-07 17:51:27 +0300

    updated description length

--- composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/matatoa/wheremystuff/Constants.kt
@@ -3,7 +3,7 @@ package com.matatoa.wheremystuff
 const val EMPTY_STRING = ""
 const val NEW_PLACE_LENGTH = 30
 const val NEW_SUB_PLACE_LENGTH = 30
-const val SUB_PLACE_DESCRIPTION_LENGTH = 60
+const val SUB_PLACE_DESCRIPTION_LENGTH = 50
 const val NEW_STUFF_LENGTH = 30
 const val MAX_SUB_PLACES = 10
 const val STOP_TIME_OUT_MILLIS = 5000L
